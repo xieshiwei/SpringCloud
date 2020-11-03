0bdf6b1bd32266496bde0e9b9ed3f931798077db
xieshiwei <xieshiwei@heycars.cn>
2020-11-03 19:14:47 +0800

    ok

--- cloud-provider-payment8001/src/main/java/com/springcloud/entities/CommonResult.java
+++ cloud-provider-payment8001/src/main/java/com/springcloud/entities/CommonResult.java
@@ -10,17 +10,39 @@ import lombok.NoArgsConstructor;
  * @Description //TODO
  * @Date 10/26/2020 11:18 PM
  **/
-@Data
-@AllArgsConstructor//全参
-@NoArgsConstructor//空参
 public class CommonResult<T> {
     private Integer code;
     private String message;
     //传进来什么类型返回什么类型
     private T data;
 
-    public CommonResult(Integer code, String message) {
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public CommonResult(Integer code, String message, T data) {
         this.code = code;
         this.message = message;
+        this.data = data;
     }
 }
--- cloud-provider-payment8001/src/main/java/com/springcloud/service/PaymentService.java
+++ cloud-provider-payment8001/src/main/java/com/springcloud/service/PaymentService.java
@@ -1,14 +1,15 @@
-package com.springcloud.dao;
+package com.springcloud.service;
 
 import com.springcloud.entities.Payment;
-import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
-@Mapper
-public interface PaymentDao {
-
+/**
+ * @auther
+ * @create 2020-02-18 10:40
+ */
+public interface PaymentService
+{
     public int create(Payment payment);
 
     public Payment getPaymentById(@Param("id") Long id);
-
 }
