27a8dc4ff9b502b4d291f622bc9e8fe52f9af70b
xieshiwei <xieshiwei@heycars.cn>
2020-11-16 15:23:35 +0800

    ok

--- cloud-consumer-order80/src/main/java/com/springcloud/config/ApplicationContextConfig.java
+++ cloud-consumer-order80/src/main/java/com/springcloud/config/ApplicationContextConfig.java
@@ -17,7 +17,7 @@ public class ApplicationContextConfig {
      * @return
      */
     @Bean
-//    @LoadBalanced
+    @LoadBalanced
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
     }
