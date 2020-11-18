a2134a0fa8293352f48ad241921c9ac44ef4d309
xieshiwei <xieshiwei@hotmail.com>
2020-11-18 22:59:52 +0800

    ok

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,19 @@
+package com.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @ClassName HystrixDashboardMain9001
+ * @Author 萌琪琪爸爸
+ * @Description //TODO
+ * @Date 2020/11/18 22:41
+ **/
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class, args);
+    }
+}
