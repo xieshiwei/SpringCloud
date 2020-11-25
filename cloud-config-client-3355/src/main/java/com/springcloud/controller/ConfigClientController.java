3be44bdae820a36b2a479cb8e385df115e1cd1c8
xieshiwei <xieshiwei@users.noreply.github.com>
2020-11-25 19:50:05 +0800

    手动刷新config-client

--- cloud-config-client-3355/src/main/java/com/springcloud/controller/ConfigClientController.java
+++ cloud-config-client-3355/src/main/java/com/springcloud/controller/ConfigClientController.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+/**
+ *自动刷新注解，需要等待一个post请求后才会刷新
+ * curl -X POST "http://localhost:3355/actuator/refresh"
+ */
 @RefreshScope
 public class ConfigClientController {
     @Value("${config.info}")
